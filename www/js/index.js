6e3b0bf301c2efdc6ec6681de05d9000271b2c3b
singleMaltMatt <mattmatte93@gmail.com>
2024-04-16 09:00:34 +0200

    completed white house

--- www/js/index.js
+++ www/js/index.js
@@ -248,7 +248,9 @@ let LARGE_HOUSE_MAP = false;
 let HOUSE_OVERGROWN_STERIODS_USED = false;
 let HOUSE_OVERGROWN_CAT_TOY = false;
 let HOUSE_OVERGROWN_EXPLORED = false;
-//
+// sceneHouseWhite
+let HOUSE_WHITE_EXPLORED = false;
+let HOUSE_WHITE_SNAKE_BITE = false;
 //
 //sceneChurch
 let CHURCH_STORY = false;
@@ -5685,7 +5687,6 @@ async function sceneResidential() {
       userControlsContainer.appendChild(leaveBtn);
     });
     
-
     leaveBtn.addEventListener("pointerup", async function () {
       // Button click check
       if (isTyping || btnRecentlyClicked) return;
@@ -5710,6 +5711,301 @@ async function sceneResidential() {
       sceneResidential();
     });
     
+  }
+
+  async function sceneHouseWhite() {
+    let gameContainer = document.querySelector(".container");
+    gameContainer.style.backgroundImage = "url(img/111white-house.png)";
+    gameContainer.style.transition = "background-image 4s ease-in-out";
+  
+    let userControlsContainer = document.querySelector(
+      ".user-controls-container"
+    );
+    let textContainer = document.querySelector(".text-container");
+    applyTypingCss(textContainer);
+  
+    // header title
+    currentSceneTitle = "White House"; // Add title
+    document.getElementById("header-title").textContent = currentSceneTitle;
+  
+    // create button
+    let forceBtn = document.createElement("button");
+    let windowBtn = document.createElement("button");
+    let leaveBtn = document.createElement("button");
+    let leaveAfterWindowBtn = document.createElement("button");
+    let walkBtn = document.createElement("button");
+    let scareBtn = document.createElement("button");
+    let runBtn = document.createElement("button");
+  
+    // set button text
+    forceBtn.textContent = "Try to force the door open";
+    windowBtn.textContent = "Look through the windows";
+    leaveBtn.textContent = "Leave";
+    leaveAfterWindowBtn.textContent = "Leave";
+    walkBtn.textContent = "Walk around the snake";
+    scareBtn.textContent = "Try and scare the snake away";
+    runBtn.textContent = "Run past the snake!";
+  
+    // add styling for button
+    applyGlassStylingGreyBtn(forceBtn);
+    applyGlassStylingGreyBtn(windowBtn);
+    applyGlassStylingGreyBtn(leaveBtn);
+    applyGlassStylingGreyBtn(leaveAfterWindowBtn);
+    applyGlassStylingGreyBtn(walkBtn);
+    applyGlassStylingGreyBtn(scareBtn);
+    applyGlassStylingGreyBtn(runBtn);
+
+    await typeText(
+      textContainer,
+      "<p>The owners of this house decided to paint it a very stark white, but that is the only noticeable feature about it. Their garden seems like it was well maintained until recently.</p>",
+      applyGlassStylingRed
+    );
+  
+    await pause();
+
+    textContainer.innerHTML = "";
+  
+    await typeText(
+      textContainer,
+      "<p>You try to open the door but find it securely locked.</p>",
+      applyGlassStylingRed
+    );
+  
+    await sleep(1500);
+  
+    // append button to user controls container
+    userControlsContainer.appendChild(forceBtn); // append relevant buttons
+  
+    forceBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+      
+      // Clear text container
+      textContainer.innerHTML = "";
+      
+      // remove button from user controls container
+      forceBtn.remove();
+      
+      // write new text
+      await typeText(
+        textContainer,
+        "<p>You try shoving against the door but it doesn't budge. It is too sturdy to force open.</p>",
+        applyGlassStylingRed
+      );
+      
+      await sleep(1500);
+      
+      // append new button
+      userControlsContainer.appendChild(windowBtn);
+      userControlsContainer.appendChild(leaveBtn);
+    });
+  
+    windowBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+      
+      // Clear text container
+      textContainer.innerHTML = "";
+      
+      // remove button from user controls container
+      windowBtn.remove();
+      leaveBtn.remove();
+      
+      // write new text
+      await typeText(
+        textContainer,
+        "<p>You try to look through the windows but they are all securely closed, with curtains drawn on the inside.</p>",
+        applyGlassStylingRed
+      );
+      
+      await sleep(1500);
+      
+      // append new button
+      userControlsContainer.appendChild(leaveAfterWindowBtn);
+    });
+  
+    leaveAfterWindowBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+      
+      // Clear text container
+      textContainer.innerHTML = "";
+      
+      // remove button from user controls container
+      leaveAfterWindowBtn.remove();
+      
+      // write new text
+      if (HOUSE_WHITE_SNAKE_BITE == true) {
+        await typeText(
+          textContainer,
+          "<p>You don't see any sign of that nasty snake anymore</p>",
+          applyGlassStylingRed
+        );
+      
+        await pause();
+
+        textContainer.innerHTML = "";
+
+        sceneResidential();
+      
+        // append new button
+        userControlsContainer.appendChild(windowBtn);
+        userControlsContainer.appendChild(leaveBtn);
+      } else {
+        await typeText(
+          textContainer,
+          "<p>As you turn around on the path to walk balk to the main road, your breath catches in your throat...<br><br>A large snake is looking straight at you, its head reared up from the gravel. you can see a twitching rabbit off to the side of the path, you must have interrupted a fresh kill.</p>",
+          applyGlassStylingRed
+        );
+
+        await sleep(1500);
+
+        userControlsContainer.appendChild(walkBtn);
+        userControlsContainer.appendChild(scareBtn);
+      }
+    });
+  
+    walkBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+      
+      // Clear text container
+      textContainer.innerHTML = "";
+      
+      // remove button from user controls container
+      walkBtn.remove();
+      scareBtn.remove();
+      
+      // write new text
+      await typeText(
+          textContainer,
+          "<p>You try to give the snake a wide berth but it slithers in front of you, hissing, and starts to come closer, venom dripping from it's fangs.</p>",
+          applyGlassStylingRed
+        );
+      
+        await sleep(1500);
+      
+        // append new button
+        userControlsContainer.appendChild(runBtn);
+    });
+  
+    scareBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+      
+      // Clear text container
+      textContainer.innerHTML = "";
+      
+      // remove button from user controls container
+      walkBtn.remove();
+      scareBtn.remove();
+      
+      // write new text
+      await typeText(
+          textContainer,
+          "<p>You raise your arms high into the air and try lunging at the snake, however it only hisses loudly and tries to rear itself higher...<br><br>It looks both angry and defensive of its meal.<br><br>You see shiny droplets of venom dripping from its exposed fangs.</p>",
+          applyGlassStylingRed
+        );
+      
+        await sleep(1500);
+      
+        // append new button
+        userControlsContainer.appendChild(runBtn);
+    });
+  
+    runBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+      
+      // Clear text container
+      textContainer.innerHTML = "";
+      
+      // remove button from user controls container
+      runBtn.remove();
+      
+      // write new text
+      await typeText(
+          textContainer,
+          "<p>You stand still for a moment, carefully watching the snake...<br>Then you dash and try to run right past the snake, if it gets any closer you will jump over it.<br><br>But the snake is faster than you expected and you have barely given two steps when you feel a sharp pain in your lower leg.<br><br>You look down to see the snake retract, barely a split second after biting you.</p>",
+          applyGlassStylingRed
+        );
+      
+      await pause();
+
+      textContainer.innerHTML = "";
+      
+      await typeText(
+          textContainer,
+          "<p>You trip from the sudden fright, and as you land on the grass, you see the snake slither back to it's kill and start to wrap around it, confident that it has dispatched the intruder.</p>",
+          applyGlassStylingRed
+        );
+      
+      await pause();
+
+      textContainer.innerHTML = "";
+
+      await typeText(
+          textContainer,
+          "<p>You look down at your leg where the snake bit you, and see two small puncture wounds, but there is barely any pain, swelling or blood.  You count yourself lucky, the snake must not have been venomous. Not to humans anyway.</p>",
+          applyGlassStylingRed
+        );
+      
+      await pause();
+
+      HOUSE_WHITE_SNAKE_BITE = true;
+
+      textContainer.innerHTML = "";
+
+      sceneResidential();
+    });
+
+    leaveBtn.addEventListener("pointerup", async function () {
+      // Button click check
+      if (isTyping || btnRecentlyClicked) return;
+      btnRecentlyClicked = true;
+      setTimeout(() => {
+        btnRecentlyClicked = false;
+      }, 1000);
+  
+      // Clear text container
+      textContainer.innerHTML = "";
+  
+      // remove button from user controls container
+      forceBtn.remove();
+      windowBtn.remove();
+      leaveBtn.remove();
+      walkBtn.remove();
+      scareBtn.remove();
+      runBtn.remove();
+  
+      HOUSE_WHITE_EXPLORED = true;
+
+      sceneResidential();
+    });
   }
   // more house scenes here below }
 }
