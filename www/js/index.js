9f4b12254bfa7f582aafd9f4b692a18ac0e41ebc
singleMaltMatt <mattmatte93@gmail.com>
2025-03-25 21:08:39 +0200

    some progress on town hall

--- www/js/index.js
+++ www/js/index.js
@@ -269,6 +269,10 @@ let CLINIC_LOCKED_IN = false;
 let CLINIC_DOOR_LEFT = false;
 let CLINIC_DOOR_RIGHT = false;
 
+//sceneHall
+let HALL_KEY = false;
+let HALL_WEAPONS = false;
+
 //|SLEEPER
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5235,12 +5239,42 @@ async function sceneHall() {
 
   // create button
   let townSquareBtn = document.createElement("button"); // create buttons for scene
+  let oldKeyBtn = document.createElement("button")
+  let enterHallBtn = document.createElement("button");
+  let upstairsBtn = document.createElement("button");
+  let openDoorBtn = document.createElement("button");
+  let tryMayorKeyBtn = document.createElement("button");
+  let returnToTownBtn = document.createElement("button");
+  let investigateCratesBtn = document.createElement("button"); //[1]
+  let investigateMayorDeskBtn = document.createElement("button"); //[2]
+  let attemptBtn = document.createElement("button");
+  let leaveBtn = document.createElement("button");
 
   // set button text
   townSquareBtn.textContent = "Go to town square";
+  oldKeyBtn.textContent = "Use old fashioned key on the lock";
+  enterHallBtn.textContent = "Enter the Town Hall";
+  upstairsBtn.textContent = "Go up the stairs";
+  openDoorBtn.textContent = "Open the door";
+  tryMayorKeyBtn.textContent = "Try the key found in the Mayor's safe";
+  returnToTownBtn.textContent = "Return to the Town Square";
+  investigateCratesBtn.textContent = "Investigate the crates";
+  investigateMayorDeskBtn.textContent = "Investigate the Mayor's desk";
+  attemptBtn.textContent = "Attempt to open one of the crates";
+  leaveBtn.textContent = "Leave before someone discovers you";
 
   // add styling for button
   applyGlassStylingGreyBtn(townSquareBtn);
+  applyGlassStylingGreyBtn(oldKeyBtn);
+  applyGlassStylingGreyBtn(enterHallBtn);
+  applyGlassStylingGreyBtn(upstairsBtn);
+  applyGlassStylingGreyBtn(openDoorBtn);
+  applyGlassStylingGreyBtn(tryMayorKeyBtn);
+  applyGlassStylingGreyBtn(returnToTownBtn);
+  applyGlassStylingGreyBtn(investigateCratesBtn);
+  applyGlassStylingGreyBtn(investigateMayorDeskBtn);
+  applyGlassStylingGreyBtn(attemptBtn);
+  applyGlassStylingGreyBtn(leaveBtn);
 
   // local conditionals
   //   let ONCEOFF = false;
@@ -5254,7 +5288,372 @@ async function sceneHall() {
   await sleep(1500);
 
   // append button to user controls container
-  userControlsContainer.appendChild(townSquareBtn);
+  if (HALL_KEY == true) {
+    userControlsContainer.appendChild(oldKeyBtn);
+  }
+  if (HALL_KEY == false) {
+    userControlsContainer.appendChild(townSquareBtn);
+  }
+
+  oldKeyBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    oldKeyBtn.remove(); // remove relevant buttons
+    townSquareBtn.remove();
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>You insert the big key into the old lock and turn it.<br>With a satisfying click, the lock pops open, and you remove the chains from the golden door handles.</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(enterHallBtn); // append relevant buttons
+  });
+
+  enterHallBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    enterHallBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>Stepping into the Town Hall you see a huge open space that is mostly dark. Beams of light coming through the high windows illuminate the rich red color of the wooden furniture in the lobby. There are potted plants here and there that are in need of some water.</p>",
+      applyGlassStylingRed
+    );
+
+    await pause();
+
+    textContainer.innerHTML = "";
+
+    await typeText(
+      textContainer,
+      "<p>You are standing in front of an ornate wooden reception desk. You can see a staircase behind the desk leading up to the first floor of the building, as well as waiting benches in neat rows around the room.<br><br>There are old fashioned wooden doors lining the sides of the ground and first floor, probably the administrative offices of the different government employees.</p>",
+      applyGlassStylingRed
+    );
+
+    await pause();
+
+    textContainer.innerHTML = "";
+
+    await typeText(
+      textContainer,
+      "<p>You notice a plaque on a door to your right which reads 'Police Station'. This strikes you as odd, but then you don't recall seeing a police building in the town. The town hall must deal with most of the issues in town , including police matters, but then where are the police officers?</p>",
+      applyGlassStylingRed
+    );
+
+    await pause();
+
+    textContainer.innerHTML = "";
+
+    await typeText(
+      textContainer,
+      "<p>The big double doors on the first floor landing catch your eye, though you cant make out the plaque on the wall next to the door, you are certain that must be the Mayor's Office.</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(upstairsBtn); // append relevant buttons
+  });
+
+  upstairsBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    upstairsBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>Just as you thought, the brass plaque next to the door reads 'Mayor Dennis Nilsen'. Perhaps you can find some answers about what is happening in this town inside.</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(openDoorBtn); // append relevant buttons
+  });
+
+  openDoorBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    openDoorBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>The door is locked tight.</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    if (LARGE_HOUSE_MAYOR_KEY == true) {
+      userControlsContainer.appendChild(tryMayorKeyBtn); // append relevant buttons
+    }
+    if (LARGE_HOUSE_MAYOR_KEY == false) {
+      userControlsContainer.appendChild(returnToTown);
+    }
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  buttonNameBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    buttonNameBtn.remove(); // remove relevant buttons
+
+    // write new text
+    await typeText(
+      textContainer,
+      "<p>NEW TEXT HERE. REMEMBER RELEVANT STYLING (NORMAL OR ITALIC) AND REMEMBER CSS</p>",
+      applyGlassStylingRed
+    );
+
+    await sleep(1500);
+
+    // append new button
+    userControlsContainer.appendChild(buttonNameBtn); // append relevant buttons
+  });
+
+  returnToTownBtn.addEventListener("pointerup", async function () {
+    // Button click check
+    if (isTyping || btnRecentlyClicked) return;
+    btnRecentlyClicked = true;
+    setTimeout(() => {
+      btnRecentlyClicked = false;
+    }, 1000);
+
+    // Clear text container
+    textContainer.innerHTML = "";
+
+    // remove button from user controls container
+    townSquareBtn.remove(); // remove relevant buttons
+    oldKeyBtn.remove();
+    enterHallBtn.remove();
+    upstairsBtn.remove();
+    openDoorBtn.remove();
+    tryMayorKeyBtn.remove();
+    returnToTown.remove();
+    investigateCratesBtn.remove();
+    investigateMayorDeskBtn.remove();
+    attemptBtn.remove();
+    leaveBtn.remove();
+
+    sceneTownSquare();
+  });
 
   townSquareBtn.addEventListener("pointerup", async function () {
     // Button click check
@@ -5269,6 +5668,16 @@ async function sceneHall() {
 
     // remove button from user controls container
     townSquareBtn.remove(); // remove relevant buttons
+    oldKeyBtn.remove();
+    enterHallBtn.remove();
+    upstairsBtn.remove();
+    openDoorBtn.remove();
+    tryMayorKeyBtn.remove();
+    returnToTown.remove();
+    investigateCratesBtn.remove();
+    investigateMayorDeskBtn.remove();
+    attemptBtn.remove();
+    leaveBtn.remove();
 
     sceneTownSquare();
   });
